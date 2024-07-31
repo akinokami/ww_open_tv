5c93bc0fb1998862d2fbb08f9a93e7ffb2c74429
erihmar <stz.erihmar@gmail.com>
2024-07-31 14:54:00 +0800

    edit orientation

--- lib/bottom_navigation_menu.dart
+++ lib/bottom_navigation_menu.dart
@@ -11,65 +11,74 @@ import 'controller/bottom_nav_controller.dart';
 class BottomNavigationMenu extends StatelessWidget {
   BottomNavigationMenu({super.key});
 
-  final TextStyle unselectedLabelStyle = TextStyle(
-      color: Colors.white.withOpacity(0.5),
-      fontWeight: FontWeight.w500,
-      fontSize: 11.sp);
-
-  final TextStyle selectedLabelStyle = TextStyle(
-      color: secondaryColor, fontWeight: FontWeight.w500, fontSize: 11.sp);
-
   buildBottomNavigationMenu(context, landingPageController) {
-    return Obx(() => MediaQuery(
-        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-        child: SizedBox(
-          height: 55.h,
-          child: BottomNavigationBar(
-            showUnselectedLabels: true,
-            showSelectedLabels: true,
-            onTap: landingPageController.changeTabIndex,
-            currentIndex: landingPageController.tabIndex.value,
-            backgroundColor: primaryColor,
-            unselectedItemColor: Colors.grey.withOpacity(0.5),
-            selectedItemColor: secondaryColor,
-            unselectedLabelStyle: unselectedLabelStyle,
-            selectedLabelStyle: selectedLabelStyle,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.tv,
-                  size: 18.w,
-                ),
-                label: 'channel'.tr,
+    return  Builder(
+      builder: (context) {
+        final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
+
+
+        final TextStyle unselectedLabelStyle = TextStyle(
+            color: Colors.white.withOpacity(0.5),
+            fontWeight: FontWeight.w500,
+            fontSize:isPortrait? 11.sp:8.sp);
+
+        final TextStyle selectedLabelStyle = TextStyle(
+            color: secondaryColor, fontWeight: FontWeight.w500, fontSize:isPortrait? 11.sp:8.sp);
+
+        return Obx(() =>MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: SizedBox(
+              height:isPortrait? 55.h:85.h,
+              child: BottomNavigationBar(
+                selectedFontSize: isPortrait ? 11.sp : 8.sp,  // Adjust font size for orientation
+                unselectedFontSize: isPortrait ? 11.sp : 8.sp,
+                showUnselectedLabels: true,
+                showSelectedLabels: true,
+                onTap: landingPageController.changeTabIndex,
+                currentIndex: landingPageController.tabIndex.value,
                 backgroundColor: primaryColor,
+                unselectedItemColor: Colors.grey.withOpacity(0.5),
+                selectedItemColor: secondaryColor,
+                unselectedLabelStyle: unselectedLabelStyle,
+                selectedLabelStyle: selectedLabelStyle,
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.tv,
+                      size: isPortrait ?18.w:15.w,
+                    ),
+                    label: 'channel'.tr,
+                    backgroundColor: primaryColor,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.search,
+                      size: isPortrait ?18.w:15.w,
+                    ),
+                    label: 'search'.tr,
+                    backgroundColor: primaryColor,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.favorite_border,
+                      size: isPortrait ?18.w:15.w,
+                    ),
+                    label: 'fav'.tr,
+                    backgroundColor: primaryColor,
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.settings,
+                      size: isPortrait ?18.w:15.w,
+                    ),
+                    label: 'settings'.tr,
+                    backgroundColor: primaryColor,
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.search,
-                  size: 18.w,
-                ),
-                label: 'search'.tr,
-                backgroundColor: primaryColor,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.favorite_border,
-                  size: 18.w,
-                ),
-                label: 'fav'.tr,
-                backgroundColor: primaryColor,
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.settings,
-                  size: 18.w,
-                ),
-                label: 'settings'.tr,
-                backgroundColor: primaryColor,
-              ),
-            ],
-          ),
-        )));
+            )));
+      }
+    );
   }
 
   @override
